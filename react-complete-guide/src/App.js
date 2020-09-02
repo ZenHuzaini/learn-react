0e993f5307dc1c8542f1238bf643d8800c0a5b82
zenhuzaini <zen96ev@gmail.com>
2020-09-02 12:43:54 +0200

    add state and props and one component file

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,15 +1,71 @@
-import React, { Component } from "react";
+import React, { Component } from "react"; // React is needed to render the component. Component is also required to liad the class
 import "./App.css";
+import Person from "./Person/Person";
 
 //first react component
 //the App component used int he index.js
 class App extends Component {
+  //the name is state becaus eit is called inside
+  state = {
+    person: [
+      { name: "inyong marinyong", age: 28 },
+      { name: "using state uus", age: 24 },
+      { name: "using sate lukasz", age: 33 },
+    ],
+    oterState: "this is other state",
+  };
+
+  switchNamehandler = () => {
+    console.log("was clicked!");
+    //DON't DO THIS : this.state.person[0].name = "zen huzaini";
+
+    //special method extended from Component
+    this.setState({
+      person: [
+        { name: "zen huzaini", age: 24 },
+        { name: "lukasz bujlo ", age: 33 },
+      ],
+    });
+  };
+
+  //method must return something htm
   render() {
+    //METHOD 1 ----------
     return (
       <div className="App">
         <h1> Hi I am react app </h1>
+        <p> I hope this one will work </p>
+        <Person name="zen" age="24" />
+        <Person name="lukasz" age="33" />
+        <Person name="inyong" age="36">
+          my hobby is gosipin orang :)
+        </Person>
+        <button onClick={this.switchNamehandler}>Switch Name </button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}>
+          my hobby is gosipin orang mulu aja :)
+        </Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>
+          my hobby is gosipin orang mulu aja :)
+        </Person>
       </div>
-    );
+    ); //we use className, not class because it is used in JS
+    //we can not add something or example <h1> after the div, because wee need to put everything into a one root element
+
+    //METHOD2 ------------
+    //WE can also use the from the React class
+    //React.createElement('div', configuration object (we can put null), 'h1', 'Hi, my name is Zen')
+    //we hope to render <div><h1>hi</h1></div>, but cannot do that, because later the <h1> will be seen as a text
+    //So, here is the process.. so we have to do this nesting
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement(
+    //     "h1",
+    //     null,
+    //     React.createElement("i", null, "bismillah bisa")
+    //   )
+    // );
+    //a bit complicated isn't it but okay // xd that's why we don't use this. but we use JSX
   }
 }
 
--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -7,5 +7,6 @@ import registerServiceWorker from "./registerServiceWorker";
 // it will render the App coponent that comes from the App.js component
 // we can also use this way to render app
 //ReactDOM.render(<h1>Try this one</h1>, document.getElementById("root"));
+//The name can later be changed of course. from App to myApp, in the import above
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
